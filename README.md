366158afe25f10d71f5c464cc79fe219ddc5b430
Patrick Doelwijt <P.Doelwijt@tools4ever.com>
2021-10-20 14:46:02 +0200

    V1

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# HelloID-Conn-Prov-Source-Magister
+# HelloID-Conn-Prov-Source-magister
+
+
+
+## Table of contents
+
+- [Introduction](#Introduction)
+- [Getting started](#Getting-started)
+  + [Connection settings](#Connection-settings)
+  + [Prerequisites](#Prerequisites)
+  + [Remarks](#Remarks)
+- [Setup the connector](Setup-The-Connector)
+- [Getting help](Getting-help)
+
+## Introduction
+The HelloID-Conn-Prov-Source-magister is used to retrieve employee and students from Magster.
+
+## Getting started
+
+### Prerequisites
+ - URL to the webservice. Example: https://tools4ever.swp.nl:8800
+ - username
+ - password
+ - layoutname
+ - magister application manager stand by for importing the layout
+
+### Actions
+| Retrieve te data in csv format.
+
+
+### Connection settings
+The following settings are required to connect to the API.
+
+| Setting     | Description |
+| ------------ | ----------- |
+| username     | The username   |
+| Password   | The password  |
+| BaseUrl    |    The URL to the Magister environment. Example: https://tools4ever.swp.nl:8800
+| layout | Name of the list in Decibel to export
+
+
+
+
+### Remarks
+ - Execute on-premises
+ - The magsiter application manager must create an layout named "tools4ever-leerlingen-actief"
+ - The magister application manager must import the contents off the layout "tools4ever-leerlingen-actief" into decibel
+ - The username must be authorized for the layout "tools4ever-leerlingen-actief"
+ - documentation can be found at https://<tenant>.swp.nl:8800/doc?
+
+
+## Getting help
+
+> _For more information on how to configure a HelloID PowerShell connector, please refer to our [documentation](https://docs.helloid.com/hc/en-us/articles/360012557600-Configure-a-custom-PowerShell-source-system) pages_
+
+> _If you need help, feel free to ask questions on our [forum](https://forum.helloid.com)_
+
+## HelloID Docs
+
+The official HelloID documentation can be found at: https://docs.helloid.com/
---
+++ persons.ps1
@@ -0,0 +1,75 @@
+$c = $configuration | ConvertFrom-Json
+$user = $c.user
+$pass = $c.pass
+$tenant = $c.tenant
+$layout = $c.layout
+
+$uri = "https://$tenant.swp.nl:8800/doc?Function=GetData&Library=Data&SessionToken=$user%3B$pass&Layout=$layout&Parameters=&Type=CSV&Encoding=ANSI"
+
+Write-Verbose -Verbose "Retrieving data from API..."
+
+try {
+    $result = Invoke-WebRequest -Method GET -Uri $uri -UseBasicParsing
+}
+catch {
+    Write-Verbose -Verbose "Error retrieving source data, aborting..."
+    Write-Verbose -Verbose $_
+    exit;
+}
+
+$data = $result.content
+
+$csv = ConvertFrom-Csv $data -Delimiter ";"
+
+Write-Verbose -Verbose "Data records converted to CSV: $($csv.Count)"
+
+if ($csv.Count -eq 0) {
+    Write-Verbose -Verbose "Empty CSV data, aborting..."
+    exit;
+}
+
+Write-Verbose -Verbose "Augmenting data..."
+
+$csv | Add-Member -MemberType NoteProperty -Name "ExternalId" -Value $null -Force
+$csv | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $null -Force
+$csv | Add-Member -MemberType NoteProperty -Name "NamingConvention" -Value $null -Force
+$csv | Add-Member -MemberType NoteProperty -Name "Contracts" -Value $null -Force
+$csv | ForEach-Object {
+    $_.ExternalId = $_.Stamnr
+    $_.DisplayName = "$($_.FIRSTNAME) $($_.PREFIX) $($_.LASTNAME) ($($_.Stamnr))" -replace "  ", " "
+    $_.NamingConvention = "B"
+
+    # Compose contract start/enddate from the study enrollment
+    if ([string]::IsNullOrEmpty($_.STUDYPERIOD)) { continue; }
+    $studyPeriodYears = $_.STUDYPERIOD.Split("- ")
+    $studyStartYear = $studyPeriodYears[0]
+    $studyEndYear = $studyPeriodYears[1]
+
+    # Create a 'contract' from the study enrollment data
+    $contract = [PSCustomObject]@{
+        ExternalId         = $_.Stamnr
+        StartDate          = "$studyStartYear/09/01"
+        EndDate            = "$studyEndYear/08/31"
+        Class              = $_.CLASS
+        ProfileCode        = $_.PROFILECODE
+        ProfileDescription = $_.PROFILEDESC
+        Study              = $_.STUDY
+        Location           = $_.LOCATION
+        Year               = $_.STUDYYEAR
+        MentorCode         = $_.CMENTORCODE
+        MentorName         = $_.CMENTORNAME
+    }
+
+    $_.Contracts = @($contract)
+}
+
+# Skip the first line containing the column headers
+$csv = $csv | Where-Object { $_.ExternalId -notlike "*Stamnr" }
+$csv = $csv | Sort-Object ExternalId -Unique
+
+Write-Verbose -Verbose "Exporting data..."
+
+$json = $csv | ConvertTo-Json -Depth 10
+$json = $json.Replace("Loginaccount.Naam", "Loginaccount_Naam")
+
+Write-Output $json
